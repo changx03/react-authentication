50ba5a960606de709a408b4e91de8ad2b9c4540b
Luke Chang <luke.x.chang@gmail.com>
2017-10-03 16:55:20 +1300

    modified:   server.js 	modified:   server/routes/auth.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('port', PORT);
 
 app.get(/.*/, (req, res) => {
---
+++ server/routes/auth.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import validator from 'validator';
+
+const router = express.Router();
+
+function validateSignupForm(res) {
+  const errors = {};
+  const isFormValid = true;
+  const message = '';
+}
